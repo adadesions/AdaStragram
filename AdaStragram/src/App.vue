13581ddd4d44a8f20e3d1d6406c94dd2c42ac954
adadesions <adadesions@gmail.com>
2020-08-27 14:51:26 +0700

    Update menu bar

--- AdaStragram/src/App.vue
+++ AdaStragram/src/App.vue
@@ -1,12 +1,32 @@
 <template>
   <div id="app">
+    <div class="ui inverted segment">
+      <div class="ui inverted secondary pointing menu">
+        <a class="active item">
+          Home
+        </a>
+        <a class="item" :href="contactLink">
+          Contact
+        </a>
+        <a class="item">
+          Friends
+        </a>
+      </div>
+    </div>
+
     <router-view/>
+
   </div>
 </template>
 
 <script>
 export default {
   name: 'App',
+  data: function() {
+    return {
+      contactLink: '/contact'
+    }
+  }
 }
 </script>
 
@@ -17,6 +37,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin: 2em
+  margin: 2em;
+  margin-top: 0;
 }
 </style>
--- AdaStragram/src/pages/ContactPage.vue
+++ AdaStragram/src/pages/ContactPage.vue
@@ -1,10 +1,22 @@
 <template>
-    <h1>Contact Page!!!</h1>
+    <div id="contact-page">
+        <h1>Contact Page!!!</h1>
+        <a :href="backLink">
+            <button>
+                Back
+            </button>
+        </a>
+    </div>
 </template>
 
 <script>
 export default {
-    name: 'ContactPage'
+    name: 'ContactPage',
+    data: function (){
+        return {
+            backLink: '/'
+        }
+    }
 }
 </script>
 
